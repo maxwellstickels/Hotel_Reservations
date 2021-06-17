082e444e83d1df7b8d61fdfd4a1a004dda2cfb54
Max Stickels <maxwellstickels@gmail.com>
2021-06-16 19:24:37 -0700

    Added starting models for rooms, reservations, and users, as well as their possible relationships. Also added some missing npm installations to package.json.

---
+++ models/Reservation.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Reservation extends Model {}
+
+Reservation.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        startDate: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        endDate: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        room_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'room',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'reservation',
+    }
+);
+
+module.exports = Reservation;
